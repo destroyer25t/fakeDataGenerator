72cabe098fa644981e181f8861e349448798d8fa
destroyer25t <js7ussr@gmail.com>
2018-02-03 00:15:04 +0500

    -Данные по дням для датчика включения лампы генерируются верно

---
+++ generate_data/HumanNeedLightGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generate_data
+{
+    public class HumanNeedLightGenerator
+    {
+        private static readonly TimeSpan wakeUpTime = new TimeSpan(8, 0, 0);
+        private static readonly TimeSpan getOutTime = new TimeSpan(8, 30, 0);
+        private static readonly TimeSpan comeBackTime = new TimeSpan(18, 45, 0);
+        private static readonly TimeSpan startSleepTime = new TimeSpan(23, 55, 0);
+
+        private TimeSpan minutesChange;
+        private static Random _rand = new Random();
+        public HumanNeedLightGenerator() {
+            minutesChange = new TimeSpan(0, _rand.Next(-10, 30), 0);
+        }
+        
+        public bool HumanNeedLight(TimeSpan time)
+        {            
+            if ((time > (wakeUpTime + minutesChange) && time < (getOutTime + minutesChange)) || (time > (comeBackTime + minutesChange) && time < (startSleepTime + minutesChange)))
+            {
+                return true;
+            }
+
+            return false;
+        }        
+    }
+}
